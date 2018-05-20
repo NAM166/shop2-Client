79169954f1fec52ad7282748bee7096f23713540
Niall <niallmcclelland166@hotmail.com>
2018-05-21 00:17:31 +0100

    Shop2 Client

--- shop2Client/Program.cs
+++ shop2Client/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -17,24 +15,24 @@ namespace shop2Client
         }
 
         public static async Task RunAsync()  // async methods return Task or Task<T>
+
         {
             try
             {
-                using (HttpClient client = new HttpClient())
+                using (HttpClient myclient = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://localhost:51931/shop2/");
-                    //replace http://localhost:51931 with the address returned when you run with Microsoft Edge
+                    myclient.BaseAddress = new Uri("http://localhost:51931");
+                    ////replace http://localhost:51931 with the address returned when you run with Microsoft Edge
                   
-
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    myclient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                     // Test get all Customers
                     Console.WriteLine("All Customers in database by descending Customer ID:");
-                    HttpResponseMessage response = await client.GetAsync("customers/all");  //customers/all is the route defined to reach the method GetAllCustomers() in the Web Api project
+                    HttpResponseMessage response = await myclient.GetAsync("customers/all");  //customers/all is the route defined to reach the method GetAllCustomers() in the Web Api project
                     if (response.IsSuccessStatusCode)                                    
                     {
                         // read results 
-                        var customers = await response.Content.ReadAsAsync<IEnumerable<CustomerClient>>(); //GetAllCustomers() returns a list of Customers
+                        var customers = await response.Content.ReadAsAsync<IEnumerable<Customer>>(); //GetAllCustomers() returns a list of Customers
                         foreach (var customer in customers)
                         {
                             Console.WriteLine(customer);
@@ -47,11 +45,11 @@ namespace shop2Client
 
                     // Test get customer with ID 1
                     Console.WriteLine("Customer 1:");
-                    response = await client.GetAsync("customer/ByCustomerId/1");
+                    response = await myclient.GetAsync("customer/ByCustomerId/1");
                     if (response.IsSuccessStatusCode)                                                   
                     {
                         // read results 
-                        var customer = await response.Content.ReadAsAsync<CustomerClient>();
+                        var customer = await response.Content.ReadAsAsync<Customer>();
                         Console.WriteLine(customer);
                     }
                     else
@@ -61,7 +59,7 @@ namespace shop2Client
 
                     // Test get Customer IDs and Name for specified keyword
                     Console.WriteLine("Customers containing Dublin in Address:");
-                    response = await client.GetAsync("customer/ByKeyword/Dublin");
+                    response = await myclient.GetAsync("customer/ByKeyword/Dublin");
                     if (response.IsSuccessStatusCode)  
                     {
                         // read results 
@@ -78,8 +76,8 @@ namespace shop2Client
 
                     // Test POST
                     Console.WriteLine("Create new Customer");
-                    CustomerClient newCustomer = new CustomerClient() { CustomerID = 6, CName = "Alex Dunne", CAddress = "26 Meadow View, Raheny, Dublin",  Phone = 0876589995  };
-                    response = await client.PostAsJsonAsync("customer/New", newCustomer);
+                    Customer newCustomer = new Customer() { CustomerID =13, CName = "Alex Dunne", CAddress = "26 Meadow View, Raheny, Dublin",  Phone = 0876589995  };
+                    response = await myclient.PostAsJsonAsync("customer/New", newCustomer);
                     if (response.IsSuccessStatusCode)
                     {
                         Console.WriteLine("Customer {0} added", newCustomer.CName);
